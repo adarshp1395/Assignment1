5bd72b1f3d62d1bc0c6a8b101a248c2b3f9f3a6d
Adarsh Patel <adarshp1395@gmail.com>
2020-02-03 23:48:33 -0500

    First Commit

---
+++ Assignment1/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            PrintPattern(n);
+
+            int n2 = 6;
+            PrintSeries(n2);
+
+            string s = "09:15:35PM";
+            string t = UsfTime(s);
+            Console.WriteLine(t);
+
+            int n3 = 110;
+            int k = 11;
+            UsfNumbers(n3, k);
+
+            string[] words = new string[] { "abcd", "dcba", "lls", "s", "sssll" };
+            PalindromePairs(words);
+
+
+        }
+
+
+        private static void PrintPattern(int n)
+        {
+            try
+            {
+                int i, j;
+                //Outer loop for number of lines in the pattern.
+                for (i = n; i >= 1; i--)
+                {
+                    // Inner loop for printing series 54321..
+                    for (j = i; j >= 1; j--)
+                    {
+                        Console.Write(j);
+                    }
+                    Console.WriteLine();
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Exception Occured while computing printPattern");
+            }
+        }
+
+
+        private static void PrintSeries(int n2)
+        {
+            try
+            {
+                //Write your code here .!!
+                /*
+                 * This method prints the following series till n terms: 1, 3, 6, 10, 15, 21……
+                     * For example, if n2 = 6, output will be
+                     * 1,3,6,10,15,21
+                     * Returns : N/A
+                     * Return type: void
+                     * Hint: Series is 1,1+2=3,1+2+3=6,1+2+3+4=10,1+2+3+4+5=15, 1+2+3+4+5+6=21……
+                 */
+                int i, num = 0;
+                List<int> result = new List<int>();
+
+
+                for (i = 1; i <= n2; i++)
+                {
+                    num = i + num;
+                    result.Add(num);
+                }
+                string resultfinal = string.Join(",", result.ToArray());
+                Console.WriteLine(resultfinal);
+
+            }
+            catch
+            {
+                Console.WriteLine("Exception Occured while computing printSeries");
+            }
+        }
+
+
+        public static string UsfTime(string s)
+        {
+            try
+            {
+                //Write your code here .!!
+            }
+            catch
+            {
+                Console.WriteLine("Exception Occured while computing UsfTime");
+            }
+            return null;
+        }
+
+
+        public static void UsfNumbers(int n3, int k)
+        {
+            try
+            {
+                // Write your code here
+            }
+            catch
+            {
+                Console.WriteLine("Exception occured while computing UsfNumbers()");
+            }
+        }
+
+
+
+        public static void PalindromePairs(string[] words)
+        {
+            try
+            {
+                // Write your code here
+            }
+            catch
+            {
+
+                Console.WriteLine("Exception occured while computing     PalindromePairs()");
+            }
+        }
+
+        public static void Stones(int n4)
+        {
+            try
+            {
+                // Write your code here
+            }
+            catch
+            {
+                Console.WriteLine("Exception occured while computing Stones()");
+            }
+        }
+
+
+    }
+}
